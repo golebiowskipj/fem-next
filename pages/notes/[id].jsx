16d54fd2e954c8262e95a25121409e846b7c4aec
golebiowskipj@gmail.com <golebiowskipj@gmail.com>
2020-11-30 17:37:37 +0100

    Add theme-ui and some components markup

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,17 +1,16 @@
-import React from "react";
+/** @jsxRuntime classic */
+/** @jsx jsx */
+import { jsx } from "theme-ui";
 import { useRouter } from "next/router";
+import Link from "next/link";
 
-const Note = () => {
+export default () => {
   const router = useRouter();
-
   const { id } = router.query;
 
   return (
-    <div>
-      <h1>Note page</h1>
-      <p>note id:{id}</p>
+    <div sx={{ variant: "containers.page" }}>
+      <h1>Note: {id} </h1>
     </div>
   );
 };
-
-export default Note;
